8d53c7ad01bd5a4fa939a7368f8aecf952442286
Kristian W <kristian.wahlroos@gmail.com>
2015-03-09 22:47:33 +0200

    new model tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 describe User do
-  it 'has the name and birthday set correctly' do
+  it "has the name and birthday set correctly" do
     user = User.new name:"Samu", birthdate:Date.new(1992,8,27)
     expect(user.name).to eq("Samu")
     expect(user.birthdate.year).to eq(1992)
   end
+
+  it "when saved, count increases" do
+    user = User.create name:"Samu", birthdate: Date.new(1992,8,27)
+    expect(User.all.count).to eq(1)
+  end
+
+  it "when removed, can't be found anymore" do
+    
+  end
+
 end
