d215313e2ffe91012def8199fd84a68e0896cbdc
samutamm <samutamm@gmail.com>
2015-03-10 16:30:54 +0200

    User-model tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,32 @@
 require 'rails_helper'
 
 describe User do
-  it "has the name and birthday set correctly" do
-    user = FactoryGirl.create(:user)
-    expect(user.name).to eq("Samu")
-    expect(user.birthdate.year).to eq(1992)
-  end
 
-  it "when saved, count increases" do
-    user = FactoryGirl.create(:user)
-    expect(User.all.count).to eq(1)
-  end
-
-  it "when removed, can't be found anymore" do
-    user = FactoryGirl.create(:user)
-    user.destroy
+  it "when no users created, database is empty." do
     expect(User.all.count).to eq(0)
   end
 
+  describe "created and" do
+    before :each do
+      @user = FactoryGirl.create(:user)
+    end
+
+    it "has the name and birthday set correctly" do
+      expect(User.first.name).to eq(@user.name)
+      expect(User.first.birthdate.year).to eq(@user.birthdate.year)
+    end
+
+    it "when saved, count increases" do
+      expect(User.all.count).to eq(1)
+    end
+
+    it "when removed, can't be found anymore" do
+      @user.destroy
+      expect(User.all.count).to eq(0)
+    end
+
+    it "to_s method returns name" do
+      expect(User.first.to_s).to eq(@user.name)
+    end
+  end
 end
