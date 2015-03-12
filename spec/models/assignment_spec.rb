d9ad000cb4284ddf06f879954688e8dd3a683c81
samutamm <samutamm@gmail.com>
2015-03-12 11:20:54 +0200

    Add more tests to test that assignment can have user(s).

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -32,5 +32,21 @@ describe Assignment do
       expect(@assignment.to_s).to eq(@assignment.name)
     end
 
+    it "can have one user." do
+      put_user_to_assignment(FactoryGirl.create(:user), @assignment)
+      expect(Assignment.first.memberships.count).to eq(1)
+      expect(Assignment.first.users.count).to eq(1)
+    end
+
+    it "can have multiple users." do
+      ["Kristian", "Samu", "Luukkainen", "Chang"].each do |username|
+        put_user_to_assignment(FactoryGirl.create(:user, name:username), @assignment)
+      end
+      expect(Assignment.first.users.count).to eq(4)
+    end
   end
+end
+
+def put_user_to_assignment(user, assignment)
+  Membership.create user_id:user.id, assignment_id:assignment.id
 end
