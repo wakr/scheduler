c02432fb94461a9c34e1cd0ed84f464b702dc82e
Kristian Wahlroos <wakr@cs.helsinki.fi>
2015-03-13 15:26:23 +0200

    fixed broken tests and added two new ones for assignment-model

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe Assignment do
-  it "no assigments in db before creating some." do
+  it " not in db before creating some." do
     expect(Assignment.all.count).to eq(0)
   end
 
-  it "assignment without name is invalid" do
+  it " without name is invalid" do
     expect(Assignment.create is_done:true).not_to be_valid
   end
 
@@ -36,9 +36,6 @@ describe Assignment do
       put_user_to_assignment(FactoryGirl.create(:user), @assignment)
       expect(Assignment.first.user_assignments.count).to eq(1)
       expect(Assignment.first.doers.count).to eq(1)
-
-    ##fix
-
     end
 
     it "can have multiple users." do
@@ -47,14 +44,30 @@ describe Assignment do
       end
       expect(Assignment.first.doers.count).to eq(4)
     end
+
+    it "can find related users." do
+      ["Kristian", "Samu", "Luukkainen", "Chang"].each do |username|
+        put_user_to_assignment(FactoryGirl.create(:user, name:username), @assignment)
+      end
+      u_1 = Assignment.all.first.doers.find_by name: "Chang"
+      u_2 = Assignment.all.first.doers.find_by name: "Samu"
+
+      expect(u_1.name).to eq("Chang")
+      expect(u_2.name).to eq("Samu")
+    end
+
+    it " when task is added to this object. It can be recovered via this object." do
+      t = Task.new name: "asd", assignment_id: @assignment.id
+      @assignment.tasks << t
+      expect(@assignment.tasks.count).to eq(1)
+      expect(@assignment.tasks.first.name).to eq("asd")
+    end
   end
 end
 
 def put_user_to_assignment(user, assignment)
 
-  asdasd = UserAssignment.create user_id:user.id, assignment_id:assignment.id
-  user.user_assignments << asdasd
-  assignment.user_assignments << asdasd
-  byebug
-  puts "hello"
+  ass = UserAssignment.create user_id:user.id, assignment_id:assignment.id
+  user.user_assignments << ass
+  assignment.user_assignments << ass
 end
