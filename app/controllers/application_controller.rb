9ce192ac20220d13893ae78fe57528752d0985bb
Kristian Wahlroos <wakr@cs.helsinki.fi>
2015-03-16 18:59:30 +0200

    added signout function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def current_user
+    return nil if session[:user_id].nil?
+    User.find(session[:user_id])
+  end
+
+  def ensure_that_user_is_signed_in
+    if current_user.nil?
+      redirect_to signin_path, notice:'you should be signed in'
+    end
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,16 @@
 class SessionsController < ApplicationController
 
   def new
+  end
 
+  def create
+      user = User.find_by username: params[:username]
+      
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to :root
   end
 
 end
