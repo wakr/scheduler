44d82b6d7b779d0d5ee3d04586a05c1aea2c4dde
samutamm <samutamm@gmail.com>
2015-03-16 18:38:38 +0200

    Validator to group and first tests for group.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ActiveRecord::Base
   has_many :memberships
   has_many :members, :through => :memberships, source: :user
+
+  validates :name, presence: true, allow_blank: false,
+            length: {minimum: 1, maximum: 18}
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Group do
+  it " does not exists in db before creating one." do
+    expect(Group.all.count).to eq(0)
+  end
+
+  it " does not accept group without name or too long name." do
+    Group.create name:"   "
+    Group.create name:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddddddddddddddddddddd"
+
+    expect(Group.count).to eq(0)
+  end
+
+
 end
