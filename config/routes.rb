1c32c129bb7367ebe7c8cfb2fffb836057547041
Kristian Wahlroos <wakr@cs.helsinki.fi>
2015-03-16 18:35:31 +0200

    added sessioncontroller and started login functions

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   resources :users
 
   root :to => "users#index"
+  get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 
   resources :memberships, only: [:new, :create, :destroy]
+
+  resource :session, only: [:new, :create, :delete]
 end
