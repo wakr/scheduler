e921a3e8633dcd3967cf32f33392d39eb1f9a21b
samutamm <samutamm@gmail.com>
2015-03-16 18:58:33 +0200

    More tests to group-model.

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Group do
   it " does not exists in db before creating one." do
-    expect(Group.all.count).to eq(0)
+    expect(Group.count).to eq(0)
   end
 
   it " does not accept group without name or too long name." do
@@ -12,5 +12,25 @@ describe Group do
     expect(Group.count).to eq(0)
   end
 
+  describe " has created groups and " do
+
+    before :each do
+      @group = FactoryGirl.create(:group)
+    end
+
+    it " allows destroy group." do
+      expect{
+        Group.first.destroy
+      }.to change{Group.count}.from(1).to(0)
+    end
+
+    it " allows user to join " do
+      u = FactoryGirl.create :user
+      expect{
+        Group.first.members << u
+      }.to change{Membership.count}.from(0).to(1)
+    end
+
+  end
 
 end
