932317225c0bbd3eaa58f4f8c56e33e63f05fe2d
samutamm <samutamm@gmail.com>
2015-03-20 15:14:02 +0200

    Add new tests.

--- spec/controllers/assignment_contoller_spec.rb
+++ spec/controllers/assignment_contoller_spec.rb
@@ -1,14 +1,35 @@
 require 'rails_helper'
 
 describe AssignmentsController do
-  describe "GET show"
+
+  before :each do
+    @assignment = FactoryGirl.create(:assignment)
+  end
+
+  describe "GET show" do
     it "assigns @tasks" do
-      assignment = FactoryGirl.create(:assignment)
       task = FactoryGirl.create(:task)
 
-      get :show, id: assignment.id
+      get :show, id: @assignment.id
       tasks = assigns(:tasks)
       expect(tasks.first).to eq(task)
+    end
+  end
+  describe "GET index" do
+    it "renders index" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+    it "response contains created assignment" do
+      get :index
+      expect(assigns(:assignments)).to eq([@assignment])
+    end
+  end
+
+  describe "GET new" do
+    it "assigns variables" do
+      get :new
+      expect(assigns(:assignment).nil?).to eq(false)
     end
   end
 
