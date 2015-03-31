d9d3e7c2ed5c2630338b067a48210f1c999906eb
samutamm <samutamm@gmail.com>
2015-03-31 23:03:47 +0300

    Add validation to membership and user_assignment to avoid that anyone saves them to db without valid information.

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,6 +2,10 @@ class Membership < ActiveRecord::Base
   belongs_to :group
   belongs_to :user
 
+  # avoid saving useless shit to db.
+  validates :group_id, presence: true
+  validates :user_id, presence: true
+
   def self.membership_exists?(user_id, group_id)
     allready_exists = self.find_mem(user_id, group_id)
     !allready_exists.nil?
--- app/models/user_assignment.rb
+++ app/models/user_assignment.rb
@@ -2,6 +2,9 @@ class UserAssignment < ActiveRecord::Base
   belongs_to :user
   belongs_to :assignment
 
-
+  # avoid saving useless shit to db.
+  validates :user_id, presence: true
+  validates :assignment_id, presence: true
+  validates :creator_id, presence: true
 
 end
