9f262ed57b28f30370d8415ffccecdeef4721e4c
Kristian W <kristian.wahlroos@gmail.com>
2015-04-19 13:53:51 +0300

    added calendar back

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,11 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
+//= require moment
+//= require fullcalendar
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
